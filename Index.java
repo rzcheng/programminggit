0474381ef4c9318763a78940548ba6acef7975a9
Ryan Cheng <rz.cheng06@gmail.com>
2023-09-08 00:21:35 -0700

    worked on index a little

--- Index.java
+++ Index.java
@@ -1,7 +1,22 @@
+import java.io.File;
+import java.util.*;
+import java.io.*;
 /**
  * Index
  */
 public class Index {
 
+    HashMap<String, String> hashes;
+
+    public Index () {
+        hashes = new HashMap<String, String>();
+    }
+
+    public void initialize () throws Exception {
+        try {
+            File objects = new File("./objects");
+            objects.mkdirs();
+        }
+    }
     
 }
