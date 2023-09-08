007abbf9f7e104cddb369edc48bcc4306b933319
Ryan Cheng <rz.cheng06@gmail.com>
2023-09-08 13:45:44 -0700

    index functionality

--- Index.java
+++ Index.java
@@ -1,6 +1,6 @@
-import java.io.File;
 import java.util.*;
 import java.io.*;
+
 /**
  * Index
  */
@@ -8,28 +8,43 @@ public class Index {
 
     HashMap<String, String> hashes;
 
-    public Index () {
+    public Index() {
         hashes = new HashMap<String, String>();
     }
 
-    public void initialize () throws Exception {
+    public void initialize() throws Exception {
 
-            File objects = new File("./objects");
-            if (!objects.exists())
-                objects.mkdirs();
-        
-            File index = new File("index");
-            if (!index.exists())
-                index.createNewFile();
+        File objects = new File("./objects");
+        if (!objects.exists())
+            objects.mkdirs();
+
+        File index = new File("index");
+        if (!index.exists())
+            index.createNewFile();
 
     }
-    
-    public void addBlob (String filename) throws Exception {
+
+    public void addBlob(String filename) throws Exception {
+
         Blob blob = new Blob(filename);
         hashes.put(filename, blob.getEncryption());
+
+        writeToIndex();
+
     }
 
-    public void writeToIndex() {
-        
+    public void writeToIndex() throws Exception {
+
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("index")));
+
+        for (String key : hashes.keySet()) {
+
+            String hash = hashes.get(key);
+            writer.println(key + " : " + hash);
+
+        }
+
+        writer.close();
     }
+
 }
